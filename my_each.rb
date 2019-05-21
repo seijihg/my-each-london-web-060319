75b4ab2c75a086aec6d79a1383675c8d42493d9b
Le H Ngo <seiji@o2.pl>
2019-05-21 21:35:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+def my_each(array)
+  i = 0
+
+  while i < array.length
+    yield i
+    i += 1
+  end
 end
+
+my_each(collection) {|n| puts n}
